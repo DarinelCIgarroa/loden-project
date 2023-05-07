355f54f386947b286432c8683e5d4afed1cf0e9a
ArcosPerez <49565559+ArcosPerez@users.noreply.github.com>
2023-05-07 17:04:41 -0600

    scroll and responsive events

--- src/components/homePages/AboutComponent.vue
+++ src/components/homePages/AboutComponent.vue
@@ -1,174 +1,166 @@
 <template>
   <q-layout>
     <q-page-container class="flex flex-center">
-      <q-page>
-        <q-card flat bordered class="my-card row no-border q-px-lg q-mt-lg">
-          <q-card-section class="text-center col-12 section-title">
-            <div class="text-h3">Acerca de nosotros</div>
-          </q-card-section>
-          <q-card-section
-            class="row flex-center text-center col-lg-6 col-md-6 col-sm-12 col-xs-12 marked-advantages"
+      <q-card
+        flat
+        bordered
+        class="my-card row no-border col-12 q-px-lg q-mt-lg"
+      >
+        <q-card-section class="text-center col-12 section-title">
+          <div class="text-h3">Acerca de nosotros</div>
+        </q-card-section>
+        <q-card-section
+          class="row flex-center text-center col-lg-6 col-md-6 col-sm-12 col-xs-12 marked-advantages"
+        >
+          <span style="font-size: 20px" class="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+          <div class="flex row flex-center text-center">
+            <div class="q-pa-md col-12">
+              <q-list class="list">
+                <q-item>
+                  <q-item-section side top>
+                    <q-icon
+                      name="fa-solid fa-check-double"
+                      color="primary"
+                    ></q-icon>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label lines="2">
+                      <span
+                        >Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                      </span>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section side top>
+                    <q-icon
+                      name="fa-solid fa-check-double"
+                      color="primary"
+                    ></q-icon>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label lines="2">
+                      <span>
+                        Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                      </span>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section side top>
+                    <q-icon
+                      name="fa-solid fa-check-double"
+                      color="primary"
+                    ></q-icon>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label lines="2">
+                      <span>
+                        Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                      </span>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section side top>
+                    <q-icon
+                      name="fa-solid fa-check-double"
+                      color="primary"
+                    ></q-icon>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label lines="2">
+                      <span>
+                        Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                      </span>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section
+          class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center home-contact"
+        >
+          <span>
+            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </span>
+          <br />
+          <q-btn
+            size="lg"
+            class="q-mt-lg"
+            outline
+            rounded
+            color="primary"
+            label="Contáctanos"
+          ></q-btn>
+        </q-card-section>
+        <q-card-section
+          class="col-lg-6 col-md-6 col-sm-12 col-xs-10 flex justify-center items-center img-content"
+        >
+          <q-img
+            src="../../statics/images/counts-img.svg"
+            style="height: 400px; max-width: 650px"
           >
-            <span style="font-size: 20px" class="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </span>
-            <div class="flex row flex-center text-center">
-              <div class="q-pa-md col-12">
-                <q-list class="list">
-                  <q-item>
-                    <q-item-section side top>
-                      <q-icon
-                        name="fa-solid fa-check-double"
-                        color="primary"
-                      ></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label lines="2">
-                        <span
-                          >Ullamco laboris nisi ut aliquip ex ea commodo
-                          consequat
-                        </span>
-                      </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section side top>
-                      <q-icon
-                        name="fa-solid fa-check-double"
-                        color="primary"
-                      ></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label lines="2">
-                        <span>
-                          Ullamco laboris nisi ut aliquip ex ea commodo
-                          consequat
-                        </span>
-                      </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section side top>
-                      <q-icon
-                        name="fa-solid fa-check-double"
-                        color="primary"
-                      ></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label lines="2">
-                        <span>
-                          Ullamco laboris nisi ut aliquip ex ea commodo
-                          consequat
-                        </span>
-                      </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section side top>
-                      <q-icon
-                        name="fa-solid fa-check-double"
-                        color="primary"
-                      ></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label lines="2">
-                        <span>
-                          Ullamco laboris nisi ut aliquip ex ea commodo
-                          consequat
-                        </span>
-                      </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </div>
+          </q-img>
+        </q-card-section>
+        <q-card-section
+          class="row flex-center text-center col-lg-6 col-md-6 col-sm-12 col-xs-12 advantages"
+        >
+          <div class="col-6 count-box advantages-info">
+            <div>
+              <q-icon class="icon" color="primary" name="far fa-smile-beam" />
+              <span class="q-ml-md">65</span>
             </div>
-          </q-card-section>
-          <q-card-section
-            class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center home-contact"
-          >
-            <span>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore
-              eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est
-              laborum.
-            </span>
-            <br />
-            <q-btn
-              size="lg"
-              class="q-mt-lg"
-              outline
-              rounded
-              color="primary"
-              label="Contáctanos"
-            ></q-btn>
-          </q-card-section>
-          <q-card-section
-            class="col-lg-6 col-md-6 col-sm-12 col-xs-10 flex justify-center items-center img-content"
-          >
-            <q-img
-              src="../../statics/images/counts-img.svg"
-              style="height: 400px; max-width: 650px"
-            >
-            </q-img>
-          </q-card-section>
-          <q-card-section class="row col-6 advantages">
-            <div class="col-6 count-box advantages-info">
-              <div>
-                <q-icon class="icon" color="primary" name="far fa-smile-beam" />
-                <span class="q-ml-md">65</span>
-              </div>
-              <p>
-                <strong>Happy Clients</strong> consequuntur voluptas nostrum
-                aliquid ipsam architecto ut.
-              </p>
+            <p>
+              <strong>Happy Clients</strong> consequuntur voluptas nostrum
+              aliquid ipsam architecto ut.
+            </p>
+          </div>
+          <div class="col-6 count-box advantages-info">
+            <div>
+              <q-icon class="icon" color="primary" name="fa-regular fa-file" />
+              <span class="q-ml-md">65</span>
             </div>
-            <div class="col-6 count-box advantages-info">
-              <div>
-                <q-icon
-                  class="icon"
-                  color="primary"
-                  name="fa-regular fa-file"
-                />
-                <span class="q-ml-md">65</span>
-              </div>
-              <p>
-                <strong>Happy Clients</strong> consequuntur voluptas nostrum
-                aliquid ipsam architecto ut.
-              </p>
+            <p>
+              <strong>Happy Clients</strong> consequuntur voluptas nostrum
+              aliquid ipsam architecto ut.
+            </p>
+          </div>
+          <div class="col-6 count-box advantages-info">
+            <div>
+              <q-icon class="icon" color="primary" name="fa-regular fa-clock" />
+              <span class="q-ml-md">65</span>
             </div>
-            <div class="col-6 count-box advantages-info">
-              <div>
-                <q-icon
-                  class="icon"
-                  color="primary"
-                  name="fa-regular fa-clock"
-                />
-                <span class="q-ml-md">65</span>
-              </div>
-              <p>
-                <strong>Happy Clients</strong> consequuntur voluptas nostrum
-                aliquid ipsam architecto ut.
-              </p>
+            <p>
+              <strong>Happy Clients</strong> consequuntur voluptas nostrum
+              aliquid ipsam architecto ut.
+            </p>
+          </div>
+          <div class="col-6 count-box advantages-info">
+            <div>
+              <q-icon
+                class="icon"
+                color="primary"
+                name="fa-regular fa-chess-queen"
+              />
+              <span class="q-ml-md">65</span>
             </div>
-            <div class="col-6 count-box advantages-info">
-              <div>
-                <q-icon
-                  class="icon"
-                  color="primary"
-                  name="fa-regular fa-chess-queen"
-                />
-                <span class="q-ml-md">65</span>
-              </div>
-              <p>
-                <strong>Happy Clients</strong> consequuntur voluptas nostrum
-                aliquid ipsam architecto ut.
-              </p>
-            </div>
-          </q-card-section>
-        </q-card>
-      </q-page>
+            <p>
+              <strong>Happy Clients</strong> consequuntur voluptas nostrum
+              aliquid ipsam architecto ut.
+            </p>
+          </div>
+        </q-card-section>
+      </q-card>
     </q-page-container>
   </q-layout>
 </template>
